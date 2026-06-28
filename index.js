2ba72bfacb5a980dc74849cfe3915fd4d7d5510d
developersaima <saimasattar6040@gmail.com>
2026-06-28 16:34:12 +0600

    Createrd all api for expenses collection

---
+++ index.js
@@ -0,0 +1,80 @@
+import dns from "node:dns";
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import cors from "cors";
+import { MongoClient, ServerApiVersion, ObjectId } from "mongodb";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.MONGO_URI;
+const port = process.env.PORT || 5000;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("expense-tracker");
+    const expensesCollection = db.collection("expenses");
+    // Create expenses
+    app.post("/expenses", async (req, res) => {
+      const result = await expensesCollection.insertOne(req.body);
+      res.send(result);
+    });
+    // Get expenses
+    app.get("/expenses", async (req, res) => {
+      const expenses = await expensesCollection.find().toArray();
+      res.send(expenses);
+    });
+    // Update expense
+    app.put("/expenses/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await expensesCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: req.body },
+      );
+      res.send(result);
+    });
+    // Delete expenses
+    app.delete("/expenses/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await expensesCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Server is running fine!");
+});
+
+
+
+if (process.env.NODE_ENV !== "production") {
+  app.listen(port, () => {
+    console.log(`Server is running on ${port}`);
+  });
+}
+
+export default app;
